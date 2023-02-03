70616725e3f78afd9b525b17cec536322bef2011
jabestin <jabestin@cloudin.email>
2023-02-03 13:35:25 +0530

    Wishlist List included

--- routes/api.php
+++ routes/api.php
@@ -36,7 +36,8 @@ Route::group(['middleware'=>['decrypt','deviceMap']], function(){
 
     Route::post('product/list', [ProductController::class,'productList']);
     Route::post('product/view', [ProductController::class,'productDetails']);
-    Route::post('product/add/wishlist', [ProductController::class,'wishlistStore']);
+    Route::post('product/wishlist/add', [ProductController::class,'wishlistStore']);
+    Route::post('product/wishlist/list', [ProductController::class,'wishlist']);
 
     Route::post('cart/store', [CartController::class,'cartStore']);
     Route::post('cart/list', [CartController::class,'cartList']);
