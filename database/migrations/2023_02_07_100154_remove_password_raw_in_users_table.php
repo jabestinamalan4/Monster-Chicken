19d0122a3b8039310b8bb24db0d11eb4185f0d30
jabestin <jabestin@cloudin.email>
2023-02-07 15:40:35 +0530

    Password raw migration updated

--- database/migrations/2023_02_07_100154_remove_password_raw_in_users_table.php
+++ database/migrations/2023_02_07_100154_remove_password_raw_in_users_table.php
@@ -26,7 +26,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('password_raw');
+            $table->string('password_raw')->nullable();
         });
     }
 };
