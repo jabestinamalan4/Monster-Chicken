1ea27fb6786c3bd6192995006364ec440479bac1
jabestin <jabestin@cloudin.email>
2023-02-08 13:43:03 +0530

    Order details included

--- database/migrations/2023_02_08_080959_create_order_details_table.php
+++ database/migrations/2023_02_08_080959_create_order_details_table.php
@@ -13,12 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('orders', function (Blueprint $table) {
+        Schema::create('order_details', function (Blueprint $table) {
             $table->id();
-            $table->integer('user_id');
-            $table->string('total_price');
-            $table->string('note');
-            $table->integer('status');
+            $table->integer('order_id');
+            $table->integer('product_id');
+            $table->integer('quantity');
             $table->timestamps();
         });
     }
@@ -30,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('orders');
+        Schema::dropIfExists('order_details');
     }
 };
--- database/seeders/EmailTemplateSeeder.php
+++ database/seeders/EmailTemplateSeeder.php
@@ -19,19 +19,13 @@ class EmailTemplateSeeder extends Seeder
                             [
                                 'event' => 'add_user',
                                 'title' => 'Account Registeration - Welcome mail',
-                                'message' => '<p>Hi [USER_NAME],</p><p>Thank you for registering with Green Bound. We hope you will enjoy using our product.</p><p>Your Mobile USER_NAME & PASSWORD as mentioned below</p><br><p>USER_NAME : [LOGIN_NAME]</p><p>Password: [LOGIN_PASS]</p><p>If you have any questions or feedback, just reply to this mail, we will get back to you.  Green Bound Team!</p>'
+                                'message' => '<p>Hi [USER_NAME],</p><p>Thank you for registering with Monster Chicken. We hope you will enjoy using our product.</p><p>Your Mobile USER_NAME & PASSWORD as mentioned below</p><br><p>USER_NAME : [LOGIN_NAME]</p><p>Password: [LOGIN_PASS]</p><p>If you have any questions or feedback, just reply to this mail, we will get back to you.  Monster Chicken Team!</p>'
                             ],
 
                             [
                                 'event' => 'forget_password',
                                 'title' => 'Account Verification - Forget Password Otp',
-                                'message' => '<p>Hi [USER_NAME],</p><p>Thank you for using with Green Bound. We hope you will enjoy using our product.</p><p>Use the following OTP to reset your password.</p><br><p>OTP is [OTP_CODE].</p><p>If you have any questions or feedback, just reply to this mail, we will get back to you.</p>'
-                            ],
-
-                            [
-                                'event' => 'agreement_mail',
-                                'title' => 'Investment Agreement',
-                                'message' => '<p>Hi [INVESTOR_NAME],</p><p>Thank you for joining with Green Bound. We have attached the investment agreement with this mail for your reference.<br><br>Thank You'
+                                'message' => '<p>Hi [USER_NAME],</p><p>Thank you for using with Monster Chicken. We hope you will enjoy using our product.</p><p>Use the following OTP to reset your password.</p><br><p>OTP is [OTP_CODE].</p><p>If you have any questions or feedback, just reply to this mail, we will get back to you.</p>'
                             ]
                         ];
 
