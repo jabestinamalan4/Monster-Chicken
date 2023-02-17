e04f298521b77bfbffb2ea323e9ec1d5645ae831
ranjithcloudin <ranjith@cloudinlabs.com>
2023-02-17 17:56:07 +0530

    Lat and longitude type change create migration

---
+++ database/migrations/2023_02_17_120750_alter_lat_and_lon_type_change_in_suppliers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('suppliers', function (Blueprint $table) {
+            $table->double('latitude')->change();
+            $table->double('longitude')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('suppliers', function (Blueprint $table) {
+            $table->decimal('latitude')->change();
+            $table->decimal('longitude')->change();
+        });
+    }
+};
